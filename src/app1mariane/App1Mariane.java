329c755ec3984d6b1937f784b838b6e01b1fa59a
maryli891 <marytta095@gmail.com>
2017-10-25 21:02:25 -0300

    ejercicio 3 operacion matematica

---
+++ src/app1mariane/App1Mariane.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package app1mariane;
+
+/**
+ *
+ * @author LC22
+ */
+public class App1Mariane 
+{
+
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) 
+    {
+      
+        // TODO code application logic here
+       
+         int numero1;
+          int numero2;
+          
+        
+          numero1=60;
+          numero2=20;
+          
+          System.out.println("Suma de numero1 y numero2:"+(numero1+numero2));
+          System.out.println("Resta de numero1 y numero2:"+(numero1-numero2));
+          System.out.println("Multiplicar de numero1 y numero2:"+(numero1*numero2));
+          System.out.println("Dividir de numero1 y numero2:"+(numero1/numero2));
+        System.out.println("Residuo de numero1 y numero2:"+(numero1%numero2));
+        
+    }
+ 
+    
+}
